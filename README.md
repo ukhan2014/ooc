8e552975174f8b0e9a52ac756a66f269e7d1d8b0
cabana <usman.na.khan@gmail.com>
2016-06-12 00:47:58 -0700

    readme and set header

---
+++ README.md
@@ -0,0 +1,2 @@
+object oriented stuff in C
+
---
+++ set/set.h
@@ -0,0 +1,13 @@
+#ifndef SET_H
+#def SET_H
+
+
+extern const void * Set;
+
+void * add(void * set, const void * element);
+void * find(cost void * set, const void * element);
+void * drop(void * set, const void * element);
+void * contains(const void * set, const vboid * element);
+
+
+#endif
